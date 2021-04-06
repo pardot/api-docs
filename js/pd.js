f9274aa50eda2708bbf4bbe63d3d115adf53a875
Francisco Sanchez <francisco.sanchez@salesforce.com>
2021-04-06 16:26:44 -0700

    Documentation deployment on Tue Apr  6 16:26:44 PDT 2021

---
+++ js/pd.js
@@ -0,0 +1,13 @@
+piAId = '70702';
+piCId = '192993';
+piHostname = 'go.pardot.com';
+
+(function() {
+	function async_load(){
+		var s = document.createElement('script'); s.type = 'text/javascript';
+		s.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + piHostname + '/pd.js';
+		var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
+	}
+	if(window.attachEvent) { window.attachEvent('onload', async_load); }
+	else { window.addEventListener('load', async_load, false); }
+})();
